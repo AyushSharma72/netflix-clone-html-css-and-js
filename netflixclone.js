bb842959b93f762f48894e7d794ed1a688d492ef
AyushSharma72 <133676532+AyushSharma72@users.noreply.github.com>
2023-06-01 18:56:11 +0530

    Add files via upload

---
+++ netflixclone.js
@@ -0,0 +1,56 @@
+
+function show1(p,q,r,s,t,u){
+    u.classList.add("subdiv");   
+    q.classList.add("subdiv");   
+    r.classList.add("subdiv");   
+    s.classList.add("subdiv");   
+    t.classList.add("subdiv");   
+    p.classList.toggle("subdiv");     
+   document.getElementById("plus1").classList.toggle("rotat")
+
+}
+function show2(p,q,r,s,t,u){
+    p.classList.add("subdiv");   
+    u.classList.add("subdiv");   
+    r.classList.add("subdiv");   
+    s.classList.add("subdiv");   
+    t.classList.add("subdiv");   
+    q.classList.toggle("subdiv");    
+    document.getElementById("plus2").classList.toggle("rotat") 
+}
+function show3(p,q,r,s,t,u){
+    p.classList.add("subdiv");   
+    q.classList.add("subdiv");   
+    u.classList.add("subdiv");   
+    s.classList.add("subdiv");   
+    t.classList.add("subdiv");   
+    r.classList.toggle("subdiv");  
+    document.getElementById("plus3").classList.toggle("rotat")     
+}
+function show4(p,q,r,s,t,u){
+    p.classList.add("subdiv");   
+    q.classList.add("subdiv");   
+    r.classList.add("subdiv");   
+    u.classList.add("subdiv");   
+    t.classList.add("subdiv");   
+    s.classList.toggle("subdiv");  
+    document.getElementById("plus4").classList.toggle("rotat")    
+}
+function show5(p,q,r,s,t,u){
+    p.classList.add("subdiv");   
+    q.classList.add("subdiv");   
+    r.classList.add("subdiv");   
+    s.classList.add("subdiv");   
+    u.classList.add("subdiv");   
+    t.classList.toggle("subdiv");   
+    document.getElementById("plus5").classList.toggle("rotat") 
+}
+function show6(p,q,r,s,t,u){
+    p.classList.add("subdiv");   
+    q.classList.add("subdiv");   
+    r.classList.add("subdiv");   
+    s.classList.add("subdiv");   
+    t.classList.add("subdiv");   
+    u.classList.toggle("subdiv");   
+    document.getElementById("plus6").classList.toggle("rotat")
+}
